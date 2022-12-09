94e9693f454092801357888522a3f204b99ad6a7
opilane <opilane@HP-CZC6297BBB>
2022-12-09 13:51:24 +0200

    Add project files.

---
+++ LihtsasFunktsioonidSohr.py
@@ -0,0 +1,31 @@
+﻿from math import * # * - kõik funktsioonid moodulist
+#import math     math.funktsioon()
+#08/12/22
+
+
+
+
+
+#print("Tere tulemst!") #выводит информацию на экран
+#nimi=input("Mis sinu nimi on?") #sisend lugemine jaoks input()->str
+#print() #tühi print
+#print(f"{nimi}, sul on väga ilus nimi!")
+#print(nimi,", sul on väga ilus nimi!") #,->tühik
+#print(nimi+", sul on väga ilus nimi!") # str+str 
+#vanus=int(input("Kui vana sa oled?")) #int(str)
+#print(nimi,"sa oled",vanus,"aastat vana")
+#vanus+=10
+#print(nimi+", 10 aastat pärast sa oled"+str(vanus)+"aastat vana")
+print()
+print("võrdse pindala ristkülik ja ring")
+a=float(input("Anna laius: "))
+b=float(input("Anna kõrgus: "))
+S=a*b #-, +, **, /, //, %
+p=2*a+2*b
+d=round(sqrt(a**2+b**2),2) #ümardamine 2 numbrit koma pärast
+print(f"Pindala = {S}\nLäbimõõt = {p}\nDiagonaal = {d}")
+print("Ringi raadius ")
+r=sqrt(S/Pi) 
+r=float(sqrt(s/pi))
+Sci=float(pi*r*2)
+print(f"pindala = {Sci}\nRadius ")
